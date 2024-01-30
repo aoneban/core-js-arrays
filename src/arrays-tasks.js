391a0ef08dc3695335470b90c291b69b191eb8e0
aoneban <torgregion1@gmail.com>
2024-01-30 11:52:39 +0100

    feat: add solved another task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -610,8 +610,12 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, item, ind) => {
+    let x = acc;
+    x = acc.concat(Array(ind + 1).fill(item));
+    return x;
+  }, []);
 }
 
 /**
